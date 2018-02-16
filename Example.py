ebece742710de1584c509128fecd064f8bc26c0f
Roman <thirtybeltmusicgroup@gmail.com>
2018-02-16 22:03:05 +0300

    example update

--- Example.py
+++ Example.py
@@ -1,7 +1,9 @@
 from parser import ParsData
 
 # get data from database
-info = "qwer DjaNGO.."
+info = "qwer DjaNGO..we98fwe9f8 Python jkfk use use more more C# l,;l,;,; ,,, \\\ HTML"
 # for example
 
 print(ParsData(info))
+#OutPut:
+#Django Python C# HTML
